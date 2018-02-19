2aa623d895834ee68f1d42f1375627f092cc8b9f
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-19 10:54:53 +0545

    added confirmation

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,6 @@ class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, :email, :address, :contact, presence: true
 end
