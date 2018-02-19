a4398d80dfd9cec876da72aeee28a12949ce20ee
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-19 19:58:31 +0545

    added enum receive type

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,6 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, :email, :address, :contact, presence: true
+
+  enum receive_type: [:aud, :crypto]
 end
---
+++ db/migrate/20180219140611_add_column_receive_type_in_account.rb
@@ -0,0 +1,5 @@
+class AddColumnReceiveTypeInAccount < ActiveRecord::Migration[5.1]
+  def change
+    add_column :accounts, :receive_type, :integer, default: 0
+  end
+end
