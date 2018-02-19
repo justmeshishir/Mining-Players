e76fe031ad8d6d0780be5aff0dac174da88f180d
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-19 10:35:07 +0545

    name, address, contact compulsory field

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, :email, :address, :contact, presence: true
 end
