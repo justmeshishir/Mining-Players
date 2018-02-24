289a8dd6c08674c3db34d9134541ec4923fe921c
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-24 09:23:25 +0545

    added scope in balance

--- app/admin/balances.rb
+++ app/admin/balances.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Balance do
-actions :all
-permit_params :crypto_name, :crypto_amount, :aud_amount, :payout_date
+  #before_action :active_assets, only: :index
+  actions :all
+  permit_params :crypto_name, :crypto_amount, :aud_amount, :payout_date
+
+  scope :unreturned_assets
+  scope :returned_assets
+  # controller do
+  #   def active_assets
+  #     print "------------------------------------------------------>>>>>>>>>>>>>"
+  #     @lists = Balance.joins(:asset).where(assets: { return_request: false})
+  #     #binding.pry
+  #     @balances = @lists.page(1).per(10)
+  #   end
+  #end
 end
--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,4 +1,6 @@
 class Balance < ApplicationRecord
 	belongs_to :asset
 	belongs_to :account
+	scope :unreturned_assets, -> {joins(:asset).where(assets: { return_request: false})}
+	scope :returned_assets, -> {joins(:asset).where(assets: { return_request: true})}
 end
