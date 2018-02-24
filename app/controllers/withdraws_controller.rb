b61dc04b4347f4e76c580c2e84f19a8db2c87810
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-24 17:00:08 +0545

    design changed and aded mailing config in production

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -3,5 +3,5 @@ class WithdrawsController < ApplicationController
 		@method = current_account.receive_type
 		@wallets = Wallet.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
 		@banks = Bank.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
-	end	
+	end
 end
