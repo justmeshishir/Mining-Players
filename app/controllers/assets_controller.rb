801d6e15907955ec13cb6ec151e34dcfd07b7402
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-24 16:31:28 +0545

    mail for request or cancel request for asset

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,11 +1,11 @@
 class AssetsController < ApplicationController
 
-	before_action :authenticate_account!, only:[:new,:create] 
+	before_action :authenticate_account!, only:[:new,:create]
 
 	def index
 		@assets = Asset.where(account_id: current_account.id).order("created_at DESC").paginate(page: params[:page], per_page: 10)
-		
-	end			
+
+	end
 
 	def create
 		@asset = current_account.assets.create(asset_params)
@@ -14,7 +14,7 @@ class AssetsController < ApplicationController
 			Balance.create(asset_id: @asset.id, account_id: @asset.account_id)
 			redirect_to assets_path
 		else
-			flash[:alert] = "Invalid input"	
+			flash[:alert] = "Invalid input"
 			render :index,status: :unprocessable_entity
 		end
 	end
@@ -35,11 +35,15 @@ class AssetsController < ApplicationController
 
 	def return
 		Asset.where(id: params[:id]).update(return_request: true , return_request_date: Date.today)
+		@asset = Asset.find(params[:id])
+		AssetMailer.request_asset(@asset).deliver
 		redirect_to assets_path
 	end
 
 	def unreturn
 		Asset.where(id: params[:id]).update(return_request: false)
+		@asset = Asset.find(params[:id])
+		AssetMailer.unreturn_request_asset(@asset).deliver
 		redirect_to assets_path
 	end
 
