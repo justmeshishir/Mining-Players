e627b6d2291bed71004315059b5534db722a302a
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-24 17:37:44 +0545

    changed email config of production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "gmail.com",
-    :user_name => "incdecoder@gmail.com",
-    :password => "sublime1234",
-    :authentication => "plain",
-    :enable_starttls_auto => true
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "incdecoder@gmail.com",
+   :password             => "sublime1234",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
   }
   config.action_mailer.default_url_options = { :host => 'mining-players.herokuapp.com' }
 end
