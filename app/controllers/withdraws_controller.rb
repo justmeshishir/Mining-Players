3f057236199b5280fd15f1a5a47589952b114bb4
Shishir Thapa <shishirthapa1@gamil.com>
2018-02-25 15:40:52 +0545

    showed withdrawn balance in view

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -4,5 +4,6 @@ class WithdrawsController < ApplicationController
 		@method = current_account.receive_type
 		@wallets = Wallet.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
 		@banks = Bank.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+		@total_bank_withdraw = Bank.where(account_id: current_account.id, confirm: true).sum("amount")
 	end
 end
