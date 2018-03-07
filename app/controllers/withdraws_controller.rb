ac2b56ed4efee3d1b71619ff6d87437b3fb963fd
Shishir Thapa <shishirthapa1@gamil.com>
2018-03-07 10:25:35 +0545

    changed withdraw to waller

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -2,7 +2,7 @@ class WithdrawsController < ApplicationController
 	before_action :authenticate_account!
 	def index
 		@method = current_account.receive_type
-		@wallets = Wallet.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+		@wallet = Wallet.where(account_id: current_account.id).last
 		@banks = Bank.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
 		@total_bank_withdraw = Bank.where(account_id: current_account.id, confirm: true).sum("amount")
 		@total_wallet_withdraw = Wallet.where(account_id: current_account.id, confirm: true).sum("amount")
