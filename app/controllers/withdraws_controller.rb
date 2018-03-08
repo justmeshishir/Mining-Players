6ed2345b4e30a838499b0732ef77a4826832f993
Shishir Thapa <shishirthapa1@gamil.com>
2018-03-08 20:17:08 +0545

    added payout history list

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -1,10 +1,13 @@
 class WithdrawsController < ApplicationController
 	before_action :authenticate_account!
 	def index
-		@method = current_account.receive_type
+		#@method = current_account.receive_type
 		@wallet = Wallet.where(account_id: current_account.id).last
-		@banks = Bank.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
-		@total_bank_withdraw = Bank.where(account_id: current_account.id, confirm: true).sum("amount")
-		@total_wallet_withdraw = Wallet.where(account_id: current_account.id, confirm: true).sum("amount")
+		#@banks = Bank.where(account_id: current_account.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+		#@total_bank_withdraw = Bank.where(account_id: current_account.id, confirm: true).sum("amount")
+		#@total_wallet_withdraw = Wallet.where(account_id: current_account.id, confirm: true).sum("amount")
+		@assets = Asset.where(account_id: current_account.id)
+		@payouts = Payout.where(asset_id: current_account.assets.ids).paginate(:page => params[:page], :per_page => 3)
+		@total_payouts = Payout.where(asset_id: current_account.assets.ids).sum("payout_amount")
 	end
 end
