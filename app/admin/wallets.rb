92e9dcabf929cfb68c489a7618455db72922f88f
Shishir Thapa <shishirthapa1@gamil.com>
2018-03-08 21:08:11 +0545

    filter wallet page in admin

--- app/admin/wallets.rb
+++ app/admin/wallets.rb
@@ -6,4 +6,11 @@ ActiveAdmin.register Wallet do
   #     Balance.where(account_id: wallet.account_id).update_all(crypto_amount: 0.0, aud_amount: 0.0)
   #   end
   # end
+  index do
+    column :id
+    column :account
+    column :address
+    column :date
+    column :lock
+  end
 end
